2b788a4dfc78ce860607f9314e5d78e679bab39a
Victor Rebiard--Crépin <victor.rebiardcrepin@viacesi.fr>
2016-06-23 19:16:36 +0200

    Debug http requests

--- main.cpp
+++ main.cpp
@@ -4,10 +4,22 @@
 #include "tools.h"
 #include "QProcess"
 #include "QDebug"
+#include "couchdb.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSettings settings("conf.ini", QSettings::IniFormat);
+    QString url = settings.value("database/url").toString();
+    QString dbname = settings.value("database/name").toString();
+
+    qDebug() << "Couchdb url: " << url;
+    qDebug() << "Couchdb database: " << dbname;
+
+    Couchdb db;
+    db.getAllDocuments(url, dbname);
+
     Analyst w;
     w.show();
     return a.exec();
